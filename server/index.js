8d9f98e80a07dd788624d1314edf95e7c9b988a5
Liu <jliu5@HFHS.ORG>
2018-05-09 15:22:14 -0400

    add express in index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 
 const PORT = process.env.npm_package_myWebServer_port || 8090;
 
-http.createServer((req, res)=>{
+app.get('*', (req, res)=>{
   console.log(`received request for ${req.url}`);
   res.writeHead(200, {'content-Type': 'text/plain'});
   res.write('hello learner');
   res.end(`received request for ${req.url}`);
-})
+});
+
+http.createServer(app)
 .listen(PORT, (error)=>{
   if(error){
     console.error(error);
