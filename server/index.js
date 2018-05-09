7f9616f1d180d3b45e63cf5c45b89d0db5a3482a
Liu <jliu5@HFHS.ORG>
2018-05-09 10:42:05 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const PORT = 8090;
+
+http.createServer((req, res)=>{
+  console.log(`received request for ${req.url}`);
+  res.writeHead(200, {'content-Type': 'text/plain'});
+  res.write('hello learner');
+  res.end(`received request for ${req.url}`);
+})
+.listen(PORT, (error)=>{
+  if(error){
+    console.error(error);
+  }else{
+    console.log(`server running at http://localhost:${PORT}/`);
+  }
+});
